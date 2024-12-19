7a3a8f59ebbb943c15d2ed88d3f8ab3ab71bbda1
Phan Thanh Trong <TV.phanthanhtrong@gmail.com>
2024-12-19 21:10:12 +0700

    create security config

---
+++ src/main/java/com/pokemonreview/api/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.pokemonreview.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static javax.management.Query.and;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
