0a075d8a60391367f2173ff5ae74368c30a929c3
Phan Thanh Trong <TV.phanthanhtrong@gmail.com>
2024-12-19 21:16:00 +0700

    add userdetailsservice

--- src/main/java/com/pokemonreview/api/security/SecurityConfig.java
+++ src/main/java/com/pokemonreview/api/security/SecurityConfig.java
@@ -2,9 +2,16 @@ package com.pokemonreview.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpRequest;
 
 import static javax.management.Query.and;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,9 +23,25 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(withDefaults());
 
         return http.build();
+    }
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
